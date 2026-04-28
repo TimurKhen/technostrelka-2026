0fccfbb28d74543146b4a54bbca81a60ea292e53
Timur Khen <timurKhen10@gmail.com>
2026-04-28 22:40:26 +0300

    capacitor config update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'HUB NN',
+  webDir: 'dist/technostrelka-2026/browser'
+};
+
+export default config;
